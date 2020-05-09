09e1cf9141ca9bd6151f9d61c4be8ff7b7e9d76a
Charles Alexander Schultz <schultz.ca@gmail.com>
2020-05-09 16:12:50 -0700

    Updates

---
+++ precache-manifest.e6dbd4cfb0779e237cafc5e3fd73a07c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b241eff8feea240535ca655d913627d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f44a6e8e10ac1eac78e9",
+    "url": "/static/js/2.f727fb5e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f727fb5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f6757714d83b9e85b07",
+    "url": "/static/js/main.4faeef06.chunk.js"
+  },
+  {
+    "revision": "2a7dc3908081dd4111f5",
+    "url": "/static/js/runtime-main.b75bdb7b.js"
+  }
+]);
